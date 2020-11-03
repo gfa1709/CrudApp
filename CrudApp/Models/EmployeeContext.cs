db8b23801f5b61ca1389281e6bde2a4d499d4b43
gfa1709 <gfa1709@gmail.com>
2020-11-03 15:33:01 +0500

    FirstCommit

---
+++ CrudApp/Models/EmployeeContext.cs
@@ -0,0 +1,19 @@
+﻿using CrudApp.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudApp.Models
+{
+    public class EmployeeContext:DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
